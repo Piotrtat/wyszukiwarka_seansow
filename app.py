1487619743583810761379a71802c53cd58a622b
Ania <annadziedziela@gmail.com>
2019-12-15 15:40:39 +0100

    'lista_filmow'

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import moje
 from movie_list_func import run_search, final
 
-
 app = Flask(__name__)
 
 
@@ -14,17 +13,19 @@ def home_page():
 @app.route('/selected')
 def all_movies():
     filmy = run_search()
-    return render_template("all_movies.html", filmy=filmy)
+    return render_template("all_movies.html", len=len(filmy), filmy=filmy)
 
 
 @app.route('/selected_movie')
 def research_movie():
-    movie = final()
+    movie = 'cos'
     return render_template("one_movie.html", one_movie=movie)
+
 
 @app.route('/helios_page')
 def reservation_mode():
     return render_template("reserve_movie.html")
+
 
 if __name__ == '__main__':
     app.run()
