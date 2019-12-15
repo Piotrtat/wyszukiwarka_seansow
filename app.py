51d84723d6277857b1be5a6db62dcaffc7a09127
Ania <annadziedziela@gmail.com>
2019-12-15 13:39:57 +0100

    'dodanie_przyciskow'

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 from flask import Flask, render_template
 import moje
+from Flask_projekt.Movie_list_func import final, new_dict, new_dict2
+
 
 app = Flask(__name__)
-# Bootstrap(app)
+
 
 @app.route('/')
 def home_page():
-    # tasks = moje
     return render_template("main_paig.html")
 
 
 @app.route('/selected')
-def research_movie():
-    filmy = moje.moje()
+def all_movies():
+    filmy = final(new_dict, new_dict2)
     return render_template("all_movies.html", filmy=filmy)
+
+
+@app.route('/selected_movie')
+def research_movie():
+    movie = "cos"
+    return render_template("one_movie.html", one_movie=movie)
+
 
 if __name__ == '__main__':
     app.run()
