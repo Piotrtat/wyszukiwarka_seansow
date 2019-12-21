5d5d9b6e7f49cd10fd6753d9b185fd725c00a233
DawidBetkier <dawid.betkier@gmail.com>
2019-12-21 10:44:31 +0100

    'próby_drop_down_list'

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import moje
 from movie_list_func import run_search, final
+import requests
 
 app = Flask(__name__)
 
@@ -25,6 +26,18 @@ def research_movie():
 @app.route('/helios_page')
 def reservation_mode():
     return render_template("reserve_movie.html")
+
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html',
+        lista_button=movies4)
+
+@app.route("/test" , methods=['GET', 'POST'])
+def test():
+    select = requests.form.get('comp_select')
+    return(str(select)) # just to see what select is
 
 
 if __name__ == '__main__':
